7784b7fcf3fd22f174627758b6a6837fba369d0e
Alessandro Alterno <alternonolimit@gmail.com>
2021-01-11 13:48:44 +0100

    Added few steps to fs example and Added .gitignore

--- fs/filesystem.ts
+++ fs/filesystem.ts
@@ -1,6 +1,6 @@
 // Testing FS Standard Lib
 // Forced to use an older version of the fs lib to use readJson and writeJson
-import { readJson, writeJson, exists } from "https://deno.land/std@0.65.0/fs/mod.ts"; 
+import { readJson, writeJson, exists, ensureDir, move } from "https://deno.land/std@0.65.0/fs/mod.ts"; 
 
 // Nous déclarons ici un objet contenant tous nos utilisateurs
 const users = [
@@ -20,12 +20,20 @@ if(fileExists) {
      // On va maintenant lire le fichier et stocker cela dans une variable
      // On va ensuite lire ces informations et les afficher dans la console
      const jsonObj = await readJson('./fs/users.json')
-     console.log(jsonObj)
+     // console.log(jsonObj)
 }
 
-if(await exists('./fs/myFile.txt')) {
-     console.log(true)
-} else {
-     console.log(false)
-}
+// Création d'un dossier txtfiles/
+const dirPath = "./fs/txtfiles/"
+await ensureDir(dirPath)
 
+// On va maintenant créer un fichier greetings.txt
+const fileName = "greetings.txt"
+await Deno.writeTextFile("./fs/"+ fileName, "Hello and welcome to this tutorial !\n Greetings from our team ! :)")
+
+// On vérifie qu'il existe bien
+if(await exists("./fs/"+ fileName)) {
+
+     // On le déplace ensuite dans notre dossier 'txtfiles' créé précédemment
+     await move("./fs/" + fileName, "./fs/txtfiles/" + fileName)
+}
