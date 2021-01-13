6848bf18ae51baac03e66bc63195dc6775c7b3dc
Alessandro Alterno <alternonolimit@gmail.com>
2021-01-13 17:42:45 +0100

    Update gitignore, added comments to webserv_mongocloud.ts

--- webserver/webserv_mongocloud.ts
+++ webserver/webserv_mongocloud.ts
@@ -3,9 +3,9 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.13.0/mod.ts";
 
 const PORT = 4000;
 const router = new Router();
-
 const app = new Application();
 
+// Déclaration de l'objet client Mongo qui nous permettra d'intéroger la BDD
 const client = new MongoClient();
 client.connectWithUri(''); // Ne pas oublier d'ajouter ici la chaine de connexion pour Mongo Cloud
 
@@ -14,6 +14,11 @@ const users = db.collection<UserSchema>('users');
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+// Cette partie permet de mettre en place une règle générale sur le contexte de toutes
+// Les requêtes du serveur web que l'on créé.
+// Ici on va également déclarer notre dossier static /public dans lequel sera situé les fichiers HTML/CSS/JS
+// Grâce à ça on pourra utiliser ces fichiers et donc les afficher.
 app.use(async (ctx) => {
     const result = ctx.request.body({ contentTypes: { json: ['form-data'] } });
     await send(ctx, ctx.request.url.pathname, {
@@ -22,6 +27,8 @@ app.use(async (ctx) => {
     });
 });
 
+// Objet UserShema qui corresponds à la TABLE "users" dans la BDD
+// Cela nous permet d'ajouter facilement des informations en base
 interface UserSchema{
     _id: { $oid: string }
     nickname: string,
@@ -32,6 +39,9 @@ router.get('/', (ctx) => {
     ctx.response.redirect('/index.html');
 })
 
+// RouterContext ici est un type qui nous permet d'accèder au contexte de la requête
+// CAD que l'on pourra gérer les requêtes d'entrée (récupérer les params / headers)
+// Et également préparer notre requête de réponse en ajoutant nos différentes infos dans celle-ci
 router.get('/users', async (ctx: RouterContext) => {
     const user = await users.find({});
     ctx.response.type = "application/json";
