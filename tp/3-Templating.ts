6757abc21d66ac156e9fe89edf451e6dcf4fbe74
AlessandroALTERNO <alessandro.alterno.e@thalesdigital.io>
2021-02-06 17:15:11 +0100

    Damn son where did you find this !

--- tp/3-Templating.ts
+++ tp/3-Templating.ts
@@ -22,8 +22,7 @@ app.use(viewEngine(oakAdapter, ejsEngine, {
 
 // Déclaration de l'objet client Mongo qui nous permettra d'intéroger la BDD
 const client = new MongoClient();
-//client.connectWithUri('mongodb://localhost:27017');
-client.connectWithUri('mongodb+srv://KalyDeno:vrrUucEhocX5mbki@cluster0.vm5ok.mongodb.net/testApp?retryWrites=true&w=majority');
+client.connectWithUri('mongodb://localhost:27017');
 
 const db = client.database('pokemon');
 const users = db.collection<UserSchema>('users');
@@ -170,7 +169,7 @@ router.post('/addPokemon', async (ctx) => {
 
 router.get('/pokedex/:name', async (ctx) => {
     const user = await users.findOne({ username : ctx.params.name})
-    
+
     if(user != null) {
         ctx.response.type = "application/json";
         ctx.render("list_pokemon", {username: user.username, pokedex : user.pokedex})
